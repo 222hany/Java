f00ccc97709cd50720bb99b80a8991aad1100915
222hany <143372305+222hany@users.noreply.github.com>
2023-08-30 19:25:48 +0900

    homework_upload_

--- chap03_If/src/com/kh/If/practice/practice4.java
+++ chap03_If/src/com/kh/If/practice/practice4.java
@@ -11,39 +11,23 @@ public class practice4 {
 		int month = sc.nextInt();
 		
 		switch (month) {
-		       case 3: 
-		    	   System.out.println(month + "월은 봄입니다.");
-		    	   break;
-		       case 4: 
-		    	   System.out.println(month + "월은 봄입니다.");
-		    	   break;
-		       case 5: 
+		       case 3:
+		       case 4:
+		       case 5:
 		    	   System.out.println(month + "월은 봄입니다.");
 		    	   break;
 		       case 6: 
-		    	   System.out.println(month + "월은 여름입니다.");
-		    	   break;
-		       case 7: 
-		    	   System.out.println(month + "월은 여름입니다.");
-		    	   break;
+		       case 7:
 		       case 8: 
 		    	   System.out.println(month + "월은 여름입니다.");
 		    	   break;
 		       case 9: 
-		    	   System.out.println(month + "월은 가을입니다.");
-		    	   break;
-		       case 10: 
-		    	   System.out.println(month + "월은 가을입니다.");
-		    	   break;
+		       case 10:
 		       case 11: 
 		    	   System.out.println(month + "월은 가을입니다.");
 		    	   break;
 		       case 12: 
-		    	   System.out.println(month + "월은 겨울입니다.");
-		    	   break;
 		       case 1: 
-		    	   System.out.println(month + "월은 겨울입니다.");
-		    	   break;
 		       case 2: 
 		    	   System.out.println(month + "월은 겨울입니다.");
 		    	   break;
