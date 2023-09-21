2ce9d58fb491a51dc6d39c675ec84738b9000f62
222hany <143372305+222hany@users.noreply.github.com>
2023-09-21 18:18:20 +0900

    modified

--- chap01_Variable/src/com/kh/variable/ReferenceVariable.java
+++ chap01_Variable/src/com/kh/variable/ReferenceVariable.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class ReferenceVariable {
 
 	public static void main(String[] args) {
-		int num = 10;  // 정수형 지역변수
-		int b = num;   // 참조변수
-		Scanner sc;    // 객체 (=인스턴스)변수
+		int num = 10;  // 정수형 지역변수 num
+		int b = num;   // 참조변수 b
+		Scanner sc;    // 객체(=인스턴스) 변수
 		final int c;   // 상수
 	}
 
