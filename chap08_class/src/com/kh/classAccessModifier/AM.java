39a9826792d54e5da9f1a43db3fa6bba45ee59c8
222hany <143372305+222hany@users.noreply.github.com>
2023-09-21 17:18:13 +0900

    modified

--- chap08_class/src/com/kh/classAccessModifier/AM.java
+++ chap08_class/src/com/kh/classAccessModifier/AM.java
@@ -9,22 +9,22 @@ public class AM {
 	
 	// 호출 메소드를 작성해서 각각 호출하기
 	public void publicMethod() {
-		System.out.println("public Method() 호출됨 / 어떤 클래스에서든 접근이 가능");
+		System.out.println("접근제한자 public");
 	}
 	
 	protected void protectedMethod() {
-		System.out.println("protected Method() 호출됨 / 같은 패키지 내에서는 접근 가능");
+		System.out.println("접근제한자 protected");
 	}
 	/*
 	default void defaultMethod() { // default는 앞에 접근제한자를 작성해 주지 않으면 기본으로 적용이 됨.
 		System.out.println("default Method() 호출됨");
 	}
-	
-	void defaultMethod() {
-		System.out.println("default Method() 호출됨 / 같은 패키지 내에서만 접근 가능");
-	}
 	*/
+	void defaultMethod() {
+		System.out.println("접근제한자 default");
+	}
+	
 	private void privateMethod() {
-		System.out.println("private Method() 호출됨 / 같은 클래스 내에서만 접근이 가능");
+		System.out.println("접근제한자 private");
 	}
 }
