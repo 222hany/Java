ea6dc1c8666fa4ec9a6504d3021daa313b565329
222hany <143372305+222hany@users.noreply.github.com>
2023-09-21 17:16:27 +0900

    network

--- chap34_Network/src/com/kh/tcpMain/TCPClient.java
+++ chap34_Network/src/com/kh/tcpMain/TCPClient.java
@@ -1,33 +1,31 @@
 package com.kh.tcpMain;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class TCPClient {
-	public static void main(String[] args) {
-		try {
+	public static void main(String[] args) throws Exception {
 			// 서버주소와 포트 번호로 소켓 생성
 			// 서버주소와 포트 번호는 내가 연결하고자 하는 포트와 동일해야 함
 			Socket socket = new Socket("localhost", 8080);
+			System.out.println("[ Server Connect ]");
 			
-			PrintWriter out = new PrintWriter(socket.getOutputStream());
-			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			// 보낼 때 코드
+			OutputStream outStream = socket.getOutputStream();
+			PrintStream printStream = new PrintStream(outStream);
+			printStream.print("HI SERVER");
+			printStream.flush();
 			
-			// 서버 되는지 테스트 메세지
-			out.println("안녕 서버?");
+			// 서버로부터 읽어옴
+			InputStream inStream = socket.getInputStream();
+			Scanner sc = new Scanner(inStream);
+			System.out.println("[Client] server : " + sc.nextLine());
+			sc.close();
 			
-			// 서버로부터 메세지 전달 되는지 확인 테스트
-			String response = in.readLine();
-			System.out.println("서버 응답 : " + response);
-			
-		} catch (UnknownHostException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+			socket.close();
+			System.out.println("BYE SERVER");
 	}
 }
